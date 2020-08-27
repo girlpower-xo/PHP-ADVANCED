8a2ded0feb5e3586013247513e4e4637d1e0b0aa
Iris Petersen <iris.petersen@student.rocva.nl>
2020-08-27 10:53:16 +0200

    opdracht 1 done

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+   
+  </head>
+  <body>
+
+    <?php
+    	
+        $voornaam['Piet'] = "Piet";
+        $voornaam['Muhammad'] ="Muhammed";
+        $voornaam['Els'] = "Els";
+
+        $achternaam['Piet'] = "kaas";
+        $achternaam['Muhammad'] = "appel";
+        $achternaam['Els'] = "aarbei";
+
+        $leeftijd['Piet'] = 18;
+        $leeftijd['Muhammad'] = 15;
+        $leeftijd['Els'] = 21;
+
+        $klas['Piet'] = "9a";
+        $klas['Muhammad'] = "8d";
+        $klas['Els'] = "3a";
+
+        
+
+        echo $voornaam['Piet']." ".$achternaam['Piet'] ." ". "is" ." ". $leeftijd['Piet'] ." ". "oud en zit in klas" ." ".  $klas['Piet'];
+
+        
+    ?>
+
+  </body>
+</html>
